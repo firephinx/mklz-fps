6e7918b813ad412691d3fae557058b741475efa8
Matthew Lee <mattlkf@outlook.com>
2017-04-22 15:14:01 -0400

    make some headway down a messy path; time to backtrack

--- sort_go/sort/src.go
+++ sort_go/sort/src.go
@@ -87,9 +87,32 @@ func generate_seq(seq []Pair, n_threads int) {
   close(ch)
 }
 
-func sample_sort(seq []Pair, n_threads int) {
-  n := len(seq)
-  n_buckets := n_threads * 2
+func count_and_partition(in []Pair, out []Pair, bucket_starts []float64, ch chan []int, done chan bool) {
+  n := len(in)
+  // The local per-bucket counts
+  counts := make([]int, len(bucket_starts))
+
+  // Which buckets the elements belong to
+  which_bucket := make([]int, n)
+
+  for i := 0; i<n;i++ {
+    which_bucket[i] = sort.SearchFloat64s(bucket_starts, in[i].x)
+    counts[which_bucket[i]]++
+  }
+
+  // Report the local bucket counts to the master
+  ch <- counts
+
+  // Wait for the master thread to update our array
+  <-done
+
+  // Write the elements in our block to the big array according to which partition it's in
+
+}
+
+func sample_sort(input []Pair, output []Pair, n_threads int) {
+  n := len(input)
+  n_buckets := n_threads
   oversample_stride := 2
   n_oversample := n_buckets * oversample_stride
 
@@ -97,14 +120,13 @@ func sample_sort(seq []Pair, n_threads int) {
   oversamples := make([]float64, n_oversample)
   for i := 0; i < n_oversample; i++ {
     random_index := hash64(uint64(i)) % uint64(n)
-    oversamples[i] = seq[random_index].x
+    oversamples[i] = input[random_index].x
   }
 
   // Sort the oversamples
   sort.Sort(sort.Float64Slice(oversamples))
 
   // Get the actual bucket start points
-
   bucket_starts := make([]float64, n_buckets)
   for i:=0; i<n_buckets;i++ {
     bucket_starts[i] = oversamples[i * oversample_stride]
@@ -113,8 +135,29 @@ func sample_sort(seq []Pair, n_threads int) {
   fmt.Println(bucket_starts)
 
   // In parallel, count how many elements are in each bucket
+  count_channel := make(chan []int)
+  done_channel := make(chan int)
+
+  n_blocks := n_threads
+  bucket_counts := make([]int, n_buckets)
+  block_len := updiv(n, n_threads)
+  for i:=0; i<n_blocks; i++ {
+    start := block_len * i
+    end := min(start + block_len, n)
+    go count_and_partition(input[start:end], output, bucket_starts, count_channel, done_channel)
+  }
   
-  // 
+  // Coordinate so the partitioning goes smoothly..
+  for i:=0; i<n_blocks; i++ {
+    local_count := <-count_channel
+    for j:=0; j<n_buckets; j++ {
+      bucket_counts[j] += local_count[j]
+      local_count[j] = bucket_counts[j] - local_count[j]
+    }
+    reply_channel <- true
+  }
+
+  // Sort within each partition
 
 }
 
@@ -123,16 +166,16 @@ func main() {
 
   n, n_threads := read_cmdline_input(os.Args)
 
-  // Allocate a slice (backed by an array)
-  seq := make([]Pair, n)
+  input := make([]Pair, n)
+  output := make([]Pair, n)
 
   // Fill in the sequence with the hash values
   time_begin := time.Now()
-  generate_seq(seq, n_threads)
+  generate_seq(input, n_threads)
   elapsed := time.Since(time_begin)
 
   fmt.Printf("Time taken to generate: %s\n", elapsed)
 
   // Sort the sequence!
-  sample_sort(seq, n_threads)
+  sample_sort(input, output, n_threads)
 }
