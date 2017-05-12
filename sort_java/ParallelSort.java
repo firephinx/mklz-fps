c99059b6a271b64b6fed84baf738de28a20490f4
Kevin Zhang <kevinleezhang@gmail.com>
2017-05-12 10:55:04 -0400

    Fixed Sample Sort.

--- sort_java/ParallelSort.java
+++ sort_java/ParallelSort.java
@@ -40,14 +40,16 @@ public class ParallelSort
       }
     }
 
-    public sortBucketsCallable(Item[] buckets, Item[] array, List<int[]> bucketLists, int numProcessors, int size, int id) {
+    public sortBucketsCallable(Item[] buckets, Item[] array, int[] bucketLists, List<int[]> bucketCounts, int numProcessors, int size, int id) {
       int numCalculate = size/numProcessors;
       int maxIdx = (id == (numProcessors-1)) ? size : (id+1)*numCalculate;
+      int[] bucketCountArray = bucketCounts.get(id);
 
       for (int i = id * numCalculate; i < maxIdx; i += 1)
       {
         int bucket = getBucket(buckets, array[i], numProcessors);
-        bucketLists.get(bucket)[i] = 1;
+        bucketLists[i] = bucket;
+        bucketCountArray[bucket] += 1;
       }
       done = 1;
     }
@@ -60,20 +62,22 @@ public class ParallelSort
         implements Callable {
     private int done;
 
-    public transferDataCallable(List<int[]> bucketLists, List<int[]> prefixSumResults, int[] bucketSizes, int[] bucketSizeOffsets, Item[] array, Item[] newArray, int id) {
-      int size = bucketSizes[id];
-      int start = bucketSizeOffsets[id];
-      int num = 0;
-      //System.out.println("Bucket " + id + " size: " + size);
-      
-      for(int j = 0; j < size; j += 1) {
-        if(bucketLists.get(id)[j] == 1) {
-          newArray[start+num] = array[j];
-          num += 1;
-          if(num == size) {
-            break;
-          }
-        }
+    public transferDataCallable(int[] bucketLists, List<int[]> bucketStarts, int[] bucketSizeOffsets, Item[] array, Item[] newArray, int numProcessors, int size, int id) {
+      int numCalculate = size/numProcessors;
+      int maxIdx = (id == (numProcessors-1)) ? size : (id+1)*numCalculate;
+      int[] bucketStart = bucketStarts.get(id);
+      int[] bucketCount = new int[numProcessors];
+
+      for(int i = 0; i < numProcessors; i += 1) {
+        bucketStart[i] += bucketSizeOffsets[i];
+      }
+
+      for (int i = id * numCalculate; i < maxIdx; i += 1)
+      {
+        int bucket = bucketLists[i];
+        int newIdx = bucketStart[bucket] + bucketCount[bucket];
+        bucketCount[bucket] += 1;
+        newArray[newIdx] = array[i];
       }
 
       done = 1;
@@ -142,21 +146,23 @@ public class ParallelSort
     double duration_making_buckets = (end_making_buckets - start_making_buckets) / 1E9;
     System.out.println("Making Buckets Duration: " + duration_making_buckets);
 
-    double start_bucket_lists = System.nanoTime();
-    List<int[]> bucketLists = new ArrayList<int[]>();
+    double start_bucket_counts = System.nanoTime();
+    int[] bucketLists = new int[size];
+    List<int[]> bucketCounts = new ArrayList<int[]>();
 
-    for (int i = 0; i < numProcessors; i += 1) {
-      int[] bucketList = new int[size];
-      bucketLists.add(bucketList);
+    for (int i = 0; i < numProcessors; i += 1)
+    {
+      int[] bucketCount = new int[numProcessors];
+      bucketCounts.add(bucketCount);
     }
 
     for (int i = 0; i < numProcessors; i += 1)
     {
-      Callable<Integer> callable = new sortBucketsCallable(buckets, array, bucketLists, numProcessors, size, i);
+      Callable<Integer> callable = new sortBucketsCallable(buckets, array, bucketLists, bucketCounts, numProcessors, size, i);
       Future<Integer> future = executor.submit(callable);
       set.add(future);
-    }
 
+    }
     while(sum < numProcessors){
       sum = 0;
       for (Future<Integer> future : set) {
@@ -168,23 +174,31 @@ public class ParallelSort
       }
     }
 
-    double end_bucket_lists = System.nanoTime();
-    double duration_bucket_lists = (end_bucket_lists - start_bucket_lists) / 1E9;
-    System.out.println("Bucket Lists Duration: " + duration_bucket_lists);
+    double end_bucket_counts = System.nanoTime();
+    double duration_bucket_counts = (end_bucket_counts - start_bucket_counts) / 1E9;
+    System.out.println("Bucket Counts Duration: " + duration_bucket_counts);
 
     double start_prefix_sums = System.nanoTime();
-    List<int[]> prefixSumResults = new ArrayList<int[]>();
-    int[] bucketSizes = new int[numProcessors];
+    List<int[]> bucketStarts = new ArrayList<int[]>();
+    int[] startingBucket = new int[numProcessors];
+    bucketStarts.add(startingBucket);
 
     for (int i = 0; i < numProcessors; i += 1) {
-      int[] bucketListCopy = Arrays.copyOf(bucketLists.get(i),size);
-      Arrays.parallelPrefix(bucketListCopy, ( x, y ) -> x + y );
-      prefixSumResults.add(bucketListCopy);
-      bucketSizes[i] = bucketListCopy[size-1];
+      int[] prevBucketStarts = bucketStarts.get(i);
+      int[] bucketStart = new int[numProcessors];
+      int[] bucketArray = bucketCounts.get(i);
+      for (int j = 0; j < numProcessors; j += 1) {
+        bucketStart[j] += prevBucketStarts[j] + bucketArray[j];
+      }
+      bucketStarts.add(bucketStart);
     }
 
-    int[] bucketSizeOffsets = Arrays.copyOf(bucketSizes,numProcessors+1);
-    Arrays.parallelPrefix (bucketSizeOffsets, ( x, y ) -> x + y );
+    int[] bucketSizes = bucketStarts.get(numProcessors);
+    int[] bucketSizeOffsets = new int[numProcessors+1];
+
+    for (int i = 0; i < numProcessors; i += 1) {
+      bucketSizeOffsets[i+1] = bucketSizeOffsets[i] + bucketSizes[i];
+    }
 
     double end_prefix_sums = System.nanoTime();
     double duration_prefix_sums = (end_prefix_sums - start_prefix_sums) / 1E9;
@@ -198,7 +212,7 @@ public class ParallelSort
 
     for (int i = 0; i < numProcessors; i += 1)
     {
-      Callable<Integer> callable = new transferDataCallable(bucketLists, prefixSumResults, bucketSizes, bucketSizeOffsets, array, newArray, i);
+      Callable<Integer> callable = new transferDataCallable(bucketLists, bucketStarts, bucketSizeOffsets, array, newArray, numProcessors, size, i);
       Future<Integer> future = executor.submit(callable);
       set.add(future);
     }
@@ -216,7 +230,7 @@ public class ParallelSort
 
     double end_transfer_data = System.nanoTime();
     double duration_transfer_data = (end_transfer_data - start_transfer_data) / 1E9;
-    System.out.println("Prefix Sum Duration: " + duration_transfer_data);
+    System.out.println("Transfer Data Duration: " + duration_transfer_data);
 
 
     sum = 0;
